927ac7d3a29513a3c0a62fb8a497fabce1e60564
Matthew Benedict <matthewbenedict@wrls10-8-13-117.local>
2023-02-18 07:27:02 -0600

    Minor bug fix

--- Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm
+++ Source/WebKit/UIProcess/ios/WKContentViewInteraction.mm
@@ -10362,7 +10362,10 @@ static BOOL applicationIsKnownToIgnoreMouseEvents(const char* &warningVersion)
 #if __IPHONE_OS_VERSION_MIN_REQUIRED >= 150000
             return [UIPointerStyle systemPointerStyle];
 #else
-        return [UIPointerStyle styleWithEffect:UIPointerHoverEffect shape:nil];
+        {
+            UITargetedPreview* p = [[UITargetedPreview alloc] initWithView:interaction.view];
+            return [UIPointerStyle styleWithEffect:[UIPointerEffect effectWithPreview:p] shape:nil];
+        }
 #endif
         return iBeamCursor();
     }
@@ -10374,7 +10377,10 @@ static BOOL applicationIsKnownToIgnoreMouseEvents(const char* &warningVersion)
 #if __IPHONE_OS_VERSION_MIN_REQUIRED >= 150000
             return [UIPointerStyle systemPointerStyle];
 #else
-            return [UIPointerStyle styleWithEffect:UIPointerHoverEffect shape:nil];
+        {
+            UITargetedPreview* p = [[UITargetedPreview alloc] initWithView:interaction.view];
+            return [UIPointerStyle styleWithEffect:[UIPointerEffect effectWithPreview:p] shape:nil];
+        }
 #endif
 
         if (cursorType == WebCore::Cursor::IBeam && _positionInformation.lineCaretExtent.contains(_positionInformation.request.point))
@@ -10384,7 +10390,10 @@ static BOOL applicationIsKnownToIgnoreMouseEvents(const char* &warningVersion)
 #if __IPHONE_OS_VERSION_MIN_REQUIRED >= 150000
     return [UIPointerStyle systemPointerStyle];
 #else
-    return [UIPointerStyle styleWithEffect:UIPointerHoverEffect shape:nil];
+    {
+        UITargetedPreview* p = [[UITargetedPreview alloc] initWithView:interaction.view];
+        return [UIPointerStyle styleWithEffect:[UIPointerEffect effectWithPreview:p] shape:nil];
+    }
 #endif
 }
 
