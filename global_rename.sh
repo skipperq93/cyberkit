d5b865c29ab5da4a9db21edf5720414b9c52070b
Matthew Benedict <matthewbenedict@wrls10-8-13-117.local>
2023-03-23 06:36:47 -0500

    Update command

--- global_rename.sh
+++ global_rename.sh
@@ -1,5 +1,16 @@
+# Rename contents of files
 git grep -l 'JavaScriptCore' | LC_ALL=C xargs sed -i '' 's|JavaScriptCore|CyberScriptCore|g'
 git grep -l 'WebCore' | LC_ALL=C xargs sed -i '' 's|WebCore|CyberCore|g'
 git grep -l 'WebKitLegacy' | LC_ALL=C xargs sed -i '' 's|WebKitLegacy|CyberKitLegacy|g'
-git grep -l 'WebKitLibraries' | LC_ALL=C xargs sed -i '' 's|WebKitLibraries|CyberKitLibraries|g'
+git grep -l 'CyberKitLibraries' | LC_ALL=C xargs sed -i '' 's|CyberKitLibraries|CyberKitLibraries|g'
 git grep -l 'WebKit' | LC_ALL=C xargs sed -i '' 's|WebKit|CyberKit|g'
+
+# Rename some actual files
+find . -name "*JavaScriptCore*" -exec rename 's|JavaScriptCore|CyberScriptCore|g' {} ";"
+find . -name "*WebCore*" -exec rename 's|WebCore|CyberCore|g' {} ";"
+find . -name "*WebKitLegacy*" -exec rename 's|WebKitLegacy|CyberKitLegacy|g' {} ";"
+find . -name "*CyberKitLibraries*" -exec rename 's|CyberKitLibraries|CyberKitLibraries|g' {} ";"
+find . -name "*WebKit*" -exec rename 's|WebKit|CyberKit|g' {} ";"
+
+# Update git to track this
+git add *
