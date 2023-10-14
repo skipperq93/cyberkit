9b745323408440696cd5b9f8a41b7efa21f4b71f
UInt2048 <46784000+UInt2048@users.noreply.github.com>
2023-10-14 13:00:15 -0500

    CyberKit v0.0.8 alpha

--- Source/WebCore/platform/cocoa/ContentFilterUnblockHandlerCocoa.mm
+++ Source/WebCore/platform/cocoa/ContentFilterUnblockHandlerCocoa.mm
@@ -98,7 +98,10 @@ RetainPtr<WebFilterEvaluator> ContentFilterUnblockHandler::unpackWebFilterEvalua
     NSError *error { nil };
     NSSet<Class> *classes = [NSSet setWithObjects:getWebFilterEvaluatorClass(), NSNumber.class, NSURL.class, NSString.class, NSMutableString.class, nil];
     NSData *data = [NSData dataWithBytesNoCopy:vector.data() length:vector.size() freeWhenDone:NO];
-    return [NSKeyedUnarchiver _strictlyUnarchivedObjectOfClasses:classes fromData:data error:&error];
+    if ([NSKeyedUnarchiver respondsToSelector:@selector(_strictlyUnarchivedObjectOfClasses:fromData:error:)])
+        return [NSKeyedUnarchiver _strictlyUnarchivedObjectOfClasses:classes fromData:data error:&error];
+    else
+        return [NSKeyedUnarchiver unarchivedObjectOfClasses:classes fromData:data error:&error];
 }
 #endif
 
