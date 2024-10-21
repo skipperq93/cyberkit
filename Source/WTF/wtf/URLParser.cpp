01e538ab9e27ec66c241b11d91692d1a929d28e4
Matthew Benedict <46784000+UInt2048@users.noreply.github.com>
2024-10-21 15:00:34 +0000

    Rewrite fakesigner script

--- Source/WTF/wtf/URLParser.cpp
+++ Source/WTF/wtf/URLParser.cpp
@@ -2965,7 +2965,9 @@ const UIDNA& URLParser::internationalDomainNameTranscoder()
     static UIDNA* encoder;
     static std::once_flag onceFlag;
     std::call_once(onceFlag, [] {
-        CFURLRef dir = CFURLCreateCopyAppendingPathComponent(nullptr, CFBundleCopyBundleURL(CFBundleGetMainBundle()), CFSTR("Frameworks/WebKit.framework/XPCServices"), true);
+        CFBundleRef webKitBundle = CFBundleGetBundleWithIdentifier(CFSTR("com.matthewbenedict.WebKit"));
+        CFURLRef bundleURL = CFBundleCopyBundleURL(webKitBundle);
+        CFURLRef dir = CFURLCreateCopyDeletingLastPathComponent(NULL, bundleURL);
         u_setDataDirectory(CFStringGetCStringPtr(CFURLGetString(dir), kCFStringEncodingUTF8));
 
         UErrorCode error = U_ZERO_ERROR;
